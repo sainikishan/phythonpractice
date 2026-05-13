10b2c6e020fa82b8a2e410038490ff9b0a651bd2
kishan <kishansaini666@gmail.com>
2026-05-13 21:42:43 +0530

    Added login API and fixed bug

--- newchai/list.py
+++ newchai/list.py
@@ -1,1 +1,15 @@
 #array
+['black', 'white', 'oolng', 'grren']
+>>> tea[1:1]  
+[]
+>>> tea[1:1]=["test","test"]
+>>> print(tea)              
+['black', 'test', 'test', 'white', 'oolng', 'grren']
+>>> tea[1:2]  
+['test']
+>>> tea[1:3]
+['test', 'test']
+>>> tea[1:3]=[]
+>>> print(tea) 
+['black', 'white', 'oolng', 'grren']
+>>> exit()^A
